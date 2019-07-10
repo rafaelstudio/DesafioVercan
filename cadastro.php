282e4ac54701f18a2fdac2ceed8333f4c789435a
Rafael nunes <studiosafeway@gmail.comcd>
2019-07-10 12:22:54 -0300

    Validacao estado e Cidade

--- cadastro.php
+++ cadastro.php
@@ -203,14 +203,14 @@
 
                 <label>            
                         <spam>UF</spam></br> 
-                        <select  name="txt_uf" id="txt_uf" value="<?php echo $linha['uf']; ?>">
-                                <option value="selecione">Selecione</option>
-                        </select>         
+                        <select name="uf" id="uf" disabled data-target="#cidade">
+                                        <option value="">Estado</option>
+                                    </select>        
                 </label>
 
                 <label>            
                         <spam>Cidade</spam></br> 
-                        <select  name="txt_cidade" id="txt_cidade" value="<?php echo $linha['cidade']; ?>">
+                        <select  name="txt_cidade" id="cidade" disabled  value="<?php echo $linha['cidade']; ?>">
                                 <option value="selecione">Selecione</option>
                         </select>        
                 </label>
@@ -244,8 +244,10 @@
     </form>    
 </div>
 <!------------Requisicao para preencher o endereco-->!>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <Script>
-       function retornaEndereco(){
+
+$(document).ready(function(){
                 $("txt_cep").focusout(function(){
                         var cep = $("#txt_cep").val();
                         cep = cep.replace("-","");
@@ -268,6 +270,91 @@
                                 }
                         });
                 });
-        };
+        });
+        var estados = [];
+
+function loadEstados(element) {
+  if (estados.length > 0) {
+    putEstados(element);
+    $(element).removeAttr('disabled');
+  } else {
+    $.ajax({
+      url: 'https://api.myjson.com/bins/enzld',
+      method: 'get',
+      dataType: 'json',
+      beforeSend: function() {
+        $(element).html('<option>Carregando...</option>');
+      }
+    }).done(function(response) {
+      estados = response.estados;
+      putEstados(element);
+      $(element).removeAttr('disabled');
+    });
+  }
+}
+
+function putEstados(element) {
+
+  var label = $(element).data('label');
+  label = label ? label : 'Estado';
+
+  var options = '<option value="">' + label + '</option>';
+  for (var i in estados) {
+    var estado = estados[i];
+    options += '<option value="' + estado.sigla + '">' + estado.nome + '</option>';
+  }
+
+  $(element).html(options);
+}
+
+function loadCidades(element, estado_sigla) {
+  if (estados.length > 0) {
+    putCidades(element, estado_sigla);
+    $(element).removeAttr('disabled');
+  } else {
+    $.ajax({
+      url: theme_url + '/assets/json/estados.json',
+      method: 'get',
+      dataType: 'json',
+      beforeSend: function() {
+        $(element).html('<option>Carregando...</option>');
+      }
+    }).done(function(response) {
+      estados = response.estados;
+      putCidades(element, estado_sigla);
+      $(element).removeAttr('disabled');
+    });
+  }
+}
+
+function putCidades(element, estado_sigla) {
+  var label = $(element).data('label');
+  label = label ? label : 'Cidade';
+
+  var options = '<option value="">' + label + '</option>';
+  for (var i in estados) {
+    var estado = estados[i];
+    if (estado.sigla != estado_sigla)
+      continue;
+    for (var j in estado.cidades) {
+      var cidade = estado.cidades[j];
+      options += '<option value="' + cidade + '">' + cidade + '</option>';
+    }
+  }
+  $(element).html(options);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadEstados('#uf');
+  $(document).on('change', '#uf', function(e) {
+    var target = $(this).data('target');
+    if (target) {
+      loadCidades(target, $(this).val());
+    }
+  });
+}, false);      
+
+
+      
 
 </Script>
